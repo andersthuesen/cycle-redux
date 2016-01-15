d95508ac70819b06df5c885d7ebfb73c510573c2
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2016-01-16 00:27:56 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export default function makeStateDriver(reducer, initialState) {
+  let state = reducer(initialState, {type: null})
+  return function(action$) {
+
+    const state$ = $action
+      .startWith(state)
+      .map(action => {
+        state = reducer(state, action)
+        return state
+      })
+
+    return state$
+
+  }
+}
