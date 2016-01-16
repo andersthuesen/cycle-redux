bd95061b1b00deb93200060be937112809e6a25c
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2016-01-16 01:13:31 +0100

    Fixed typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default function makeStateDriver(reducer, initialState) {
   let state = reducer(initialState, {type: null})
   return function(action$) {
 
-    const state$ = $action
+    const state$ = action$
       .startWith(state)
       .map(action => {
         state = reducer(state, action)
